e6991388ade70f7e50af0d905196cd392159e7d3
TechBug2012 <mattbstanciu@gmail.com>
2018-07-29 09:16:02 -0400

    cleaned up a bit

--- paragraph.js
+++ paragraph.js
@@ -7,14 +7,13 @@ var ctx = canvas.getContext("2d");
 var img = document.createElement("img");
 form.onsubmit = function(event) {
   event.preventDefault();
-  console.log(formInput.value);
-  draw(formInput.value);
+  draw();
   formInput.value = "";
 };
 
 img.src = "https://cdn.glitch.com/698488f3-b848-4b11-be98-9b7babb373a2%2Fsparkly-white-light-particles-moving-across-a-red-purple-blue-gradient-background-imitating-night-sky-full-of-stars_h2ctphk0_thumbnail-full01.png?1532784996284";
 
-function draw(value) {
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.save();
   ctx.beginPath();
@@ -31,31 +30,28 @@ function draw(value) {
 }
 
 function wrap(text, maxWidth) {
-    var words = text.split(' '),
-        lines = [],
+  var words = text.split(' '),
+      lines = [],
+      line = "";
+  if (ctx.measureText(text).width < maxWidth) { return [text]; }
+  while (words.length > 0) {
+    while (ctx.measureText(words[0]).width >= maxWidth) {
+      var tmp = words[0];
+      words[0] = tmp.slice(0, -1);
+      if (words.length > 1) {
+        words[1] = tmp.slice(-1) + words[1];
+      } else {
+        words.push(tmp.slice(-1));
+      }
+    }
+    if (ctx.measureText(line + words[0]).width < maxWidth) {
+      line += words.shift() + " ";
+    } else {
+        lines.push(line);
         line = "";
-    if (ctx.measureText(text).width < maxWidth) {
-        return [text];
     }
-    while (words.length > 0) {
-        while (ctx.measureText(words[0]).width >= maxWidth) {
-            var tmp = words[0];
-            words[0] = tmp.slice(0, -1);
-            if (words.length > 1) {
-                words[1] = tmp.slice(-1) + words[1];
-            } else {
-                words.push(tmp.slice(-1));
-            }
-        }
-        if (ctx.measureText(line + words[0]).width < maxWidth) {
-            line += words.shift() + " ";
-        } else {
-            lines.push(line);
-            line = "";
-        }
-        if (words.length === 0) {
-            lines.push(line);
-        }
-    }
-    return lines;
+    if (words.length === 0) { lines.push(line); }
+  }
+
+  return lines;
 }
